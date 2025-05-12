7664dc5d44cf3ff8da484e221b03b3c61660f002
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-05-12 10:43:33 -0400

    add model methods for Choice and Question

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,7 @@
-from django.db import models
-
-# Create your models here.
+import datetime
 
 from django.db import models
+from django.utils import timezone
 
 # Each model is represented by a class that subclasses django.db.models.Model
 
@@ -15,12 +14,20 @@ from django.db import models
 
 # You can use an optional first positional argument to a Field to designate a human-readable name. That’s used in a couple of introspective parts of Django, and it doubles as documentation. If this field isn’t provided, Django will use the machine-readable name. In this example, we’ve only defined a human-readable name for Question.pub_date. For all other fields in this model, the field’s machine-readable name will suffice as its human-readable name.
 
+# It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
